57a7f1173cc4ea0dc025f6652f65c3ee19a5024a
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-01 17:13:06 -0300

    UI done to show collectables

---
+++ PlataformTest/Assets/Scripts/UI/CollectableCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableCounter : SingletonBase<CollectableCounter>
+{
+    [SerializeField]
+    Text collectableText;
+    int numberOfCollectables;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        DontDestroyOnLoad(gameObject);
+        numberOfCollectables = 0;
+        collectableText.text = numberOfCollectables.ToString();
+    }
+
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    public void SetNumberOfCollectables(int _numberOfCollectables) 
+    {
+        numberOfCollectables = _numberOfCollectables;
+        collectableText.text = numberOfCollectables.ToString();
+    }
+
+    public int GetNumberOfCollectables() 
+    {
+        return numberOfCollectables;
+    }
+}
