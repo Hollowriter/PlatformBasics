d6df5ac55f494aca09128a0ed2db606dbd7104f3
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-02 11:44:07 -0300

    coin collectable done

---
+++ PlataformTest/Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : SingletonBase<PlayerCollisions>
+{
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            if (collision.gameObject.activeInHierarchy)
+            {
+                CollectableCounter.instance.SetNumberOfCollectables(CollectableCounter.instance.GetNumberOfCollectables() + 1);
+                collision.gameObject.SetActive(false);
+            }
+        }
+    }
+}
