aa455d48d624998d9b1cd3be60e18850303154f5
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-02 16:21:02 -0300

    hecho el script para el pendulo

---
+++ PlataformTest/Assets/Scripts/Player/PlayerPendulum.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPendulum : SingletonBase<PlayerPendulum>
+{
+    [SerializeField]
+    GameObject parentVehicle;
+    Vector3 rotationVector;
+    [SerializeField]
+    float maxTimer;
+    [SerializeField]
+    float speed;
+    int phase;
+    bool colliding;
+    float timer;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        rotationVector = Vector3.zero;
+        phase = 0;
+        timer = 0;
+        colliding = false;
+    }
+
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    void PendularMovement() 
+    {
+        timer += Time.fixedDeltaTime;
+        switch (phase)
+        {
+            case 0:
+                rotationVector.z = speed * (1 - timer);
+                break;
+            case 1:
+                rotationVector.z = -speed * timer;
+                break;
+            case 2:
+                rotationVector.z = -speed * (1 - timer);
+                break;
+            case 3:
+                rotationVector.z = speed * timer;
+                break;
+        }
+        transform.Rotate(rotationVector);
+    }
+
+    void PhaseController() 
+    {
+        if (timer > maxTimer)
+        {
+            phase++;
+            phase %= 4;
+            timer = 0f;
+        }
+    }
+}
