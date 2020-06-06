812329e4b0e8c535dfd6ebe9eded7b4de2e31b4e
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-06 20:37:42 -0300

    bullet made prefab

---
+++ PlataformTest/Assets/Scripts/Enemies/EnemyMovementTypes/EnemyRanged.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRanged : EnemyMovement
+{
+    [SerializeField]
+    int bulletsPerSecond;
+    int bulletTimer;
+    protected override void ElementAwake()
+    {
+        base.ElementAwake();
+        target = PlayerMovement.instance.gameObject.GetComponent<Transform>();
+        attacking = false;
+        direction = false;
+        bulletTimer = 0;
+    }
+
+    private void Awake()
+    {
+        ElementAwake();
+    }
+
+    protected override void AttackPlayer()
+    {
+    }
+
+    protected override void ElementBehave()
+    {
+        if (attacking)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Update()
+    {
+        ElementBehave();
+    }
+}
