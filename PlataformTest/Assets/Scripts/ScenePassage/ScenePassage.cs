dad8c2473f223b4e35e50366e74e87c9fb83abe0
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-07 18:20:28 -0300

    scene passage done

---
+++ PlataformTest/Assets/Scripts/ScenePassage/ScenePassage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePassage : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+    const string menuSceneName = "Menu";
+
+    void DeleteAndReset() 
+    {
+        Destroy(GameObject.Find("Main Camera"));
+        Destroy(GameObject.Find("InputManager"));
+        Destroy(GameObject.Find("Player"));
+        Destroy(GameObject.Find("Canvas"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            if (sceneName == menuSceneName)
+                DeleteAndReset();
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
